00e179cefd540d99f782a0047aa6b58192e112bc
Melwyn <melwynt@gmail.com>
2021-12-11 21:07:03 +0100

    :sparkles: Add React Dropzone Uploader (#2)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Dropzone from 'react-dropzone-uploader';
+import 'react-dropzone-uploader/dist/styles.css';
 
 const App = () => {
-  return <div>Upload App</div>;
+  return <Standard />;
+};
+
+const Standard = () => {
+  const getUploadParams = () => {
+    return { url: 'https://httpbin.org/post' };
+  };
+
+  const handleChangeStatus = ({ meta }, status) => {
+    // console.log(status, meta);
+  };
+
+  const handleSubmit = (files) => {
+    const f = files[0];
+    console.log(f.file);
+    f.remove();
+  };
+
+  return (
+    <Dropzone
+      getUploadParams={getUploadParams}
+      onChangeStatus={handleChangeStatus}
+      onSubmit={handleSubmit}
+      maxFiles={1}
+      multiple={false}
+      canCancel={false}
+      accept="application/zip"
+      inputContent="Click to select Zip File or Drop Zip File"
+      styles={{ dropzone: { minHeight: 200, maxHeight: 250 } }}
+    />
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
