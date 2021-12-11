17f5a6fcce7dc900ecac411fb2405b2661eef10f
Melwyn Turbant <melwyn@mimpix.co.uk>
2021-12-11 20:47:48 +0100

    :page_facing_up: Create app skeleton

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return <div>Upload App</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
